6115b9fd87f015488fc4040b8170dada453792e7
Vlasta Rencova <rencovav@seznam.cz>
2021-04-05 16:23:54 +0200

    Created Dictionary Component + search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
+import "./Dictionary.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <header className="App-header"></header>
+      <Dictionary />
+      <footer>
+        Coded by Vlasta Řenčová, see the code on{" "}
+        <a href="https://github.com/rencovav/dictionary-react">Git Hub</a>
+      </footer>
     </div>
   );
 }
-
-
