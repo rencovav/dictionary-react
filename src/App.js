1b8b73f7de923017399adf5dc2d76bbda3505798
Vlasta Rencova <rencovav@seznam.cz>
2021-04-06 17:17:47 +0200

    Added CSS styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <Dictionary />
+      <Dictionary defaultKeyword="dictionary" />
       <footer>
         Coded by Vlasta Řenčová, see the code on{" "}
         <a href="https://github.com/rencovav/dictionary-react">Git Hub</a>
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Synonyms.css";
 
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <div className="Synonyms">
-        <strong>Synonyms: </strong> 
+      <ul className="Synonyms">
+        
         {props.synonyms.map(function (synonym, index) {
-          return <span key={index}>{synonym}, </span>;
+          return <li key={index}>{synonym} </li>;
         })}
-      </div>
+      </ul>
     );
   } else {
     return null;
